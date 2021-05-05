02e52bbfe819c708008495c8152b8769f292c87e
Luiz Marques <marqsuiz@gmail.com>
2021-05-05 13:05:39 -0300

    Add project files.

---
+++ SmartSchool.API/Controllers/ProfessorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+     
+        public ProfessorController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Professores: Marta, Paula, Lucas, Rafa");
+        }
+    }
+}
---
+++ SmartSchool.API/Models/AlunoDisciplina.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Models
+{
+    public class AlunoDisciplina
+    {
+        public AlunoDisciplina()
+        {
+
+        }
+
+        public AlunoDisciplina(int alunoId, Aluno aluno)
+        {
+            AlunoId = alunoId;
+            Aluno = aluno;
+        }
+
+        public int AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+        public int DisciplinaId { get; set; }
+        public Disciplina Disciplina { get; set; }
+    }
+}
